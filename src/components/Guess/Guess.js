cfb9ae6a3276687079a7458a38a58addf02dbb1b
Pete Hotchkiss <pete@pheekaboo.io>
2023-02-20 17:10:21 +0000

    column loop doesn't throw key error to console

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,11 +5,8 @@ function Guess({guess}) {
 
   const letters = (guess && guess.split('')) ?? ["","","","",""];
 
-  console.log("guess is", guess);
-  
   return <p className="guess">
-    { letters.map(l => <span className="cell">{l}</span>)}
-    
+    { letters.map(l => <span className="cell" key={crypto.randomUUID()}>{l}</span>)}
   </p>
 }
 
