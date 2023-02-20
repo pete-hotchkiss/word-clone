1c9883f74c9373fd23577b89208b6d512c6fb050
Pete Hotchkiss <pete@pheekaboo.io>
2023-02-20 17:45:36 +0000

    nuke some console logs

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,13 +5,8 @@ import { checkGuess } from "../../game-helpers";
 
 function Guess({value, answer}) {
 
-  // const result = (value && value.split('')) ?? Array(5).fill("");
-
-
   let result = checkGuess(value, answer) ?? Array(5).fill("");
-
-  console.log("Result", answer, result);
-
+  
   return <p className="guess">
     { result.map(
       ({letter, status}) => 
