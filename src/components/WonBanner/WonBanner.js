a1322d413f7a0abcfddb193d2a11e9248c637d20
Pete Hotchkiss <pete@pheekaboo.io>
2023-02-21 12:23:09 +0000

    better whitespace

--- src/components/WonBanner/WonBanner.js
+++ src/components/WonBanner/WonBanner.js
@@ -5,8 +5,9 @@ import Banner  from "../Banner";
 function WonBanner({totalGuesses, handleRestart}) {
   return <Banner status="happy" handleRestart={handleRestart}>
     <p>
-      <strong>Congratulations!</strong> Got it in 
-      <strong> {totalGuesses === 1 ? `1 guess` : `${totalGuesses} guesses`}</strong>.
+      <strong>Congratulations!</strong> Got it in
+      {' '} 
+      <strong>{totalGuesses === 1 ? `1 guess` : `${totalGuesses} guesses`}</strong>.
     </p>
   </Banner>;
 }
